0a4bde99f34a2bf8745392a2a2f7d6c803d9dd3c
SystemCall02 <127385528+SystemCall02@users.noreply.github.com>
2024-06-04 16:09:23 +0800

    属性查询单功能实现
    属性查询单功能实现

--- src/main/java/com/idme/minibom/Controller/PartController.java
+++ src/main/java/com/idme/minibom/Controller/PartController.java
@@ -1,101 +1,56 @@
 package com.idme.minibom.Controller;
 
-import com.alibaba.fastjson.JSONArray;
-import com.huawei.innovation.rdm.coresdk.basic.dto.ObjectReferenceParamDTO;
-import com.huawei.innovation.rdm.san2.bean.enumerate.AssemblyMode;
-import com.huawei.innovation.rdm.san2.bean.enumerate.PartSource;
+import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdDecryptDTO;
+import com.huawei.innovation.rdm.coresdk.extattrmgmt.dto.EXAValueParamDTO;
+import com.huawei.innovation.rdm.coresdk.extattrmgmt.dto.EXAValueViewDTO;
 import com.huawei.innovation.rdm.san2.delegator.PartDelegator;
-import com.huawei.innovation.rdm.san2.dto.entity.PartBranchCreateDTO;
 import com.huawei.innovation.rdm.san2.dto.entity.PartCreateDTO;
-import com.huawei.innovation.rdm.san2.dto.entity.PartMasterCreateDTO;
 import com.huawei.innovation.rdm.san2.dto.entity.PartViewDTO;
-import com.idme.minibom.pojo.DTO.PartDTO;
+import com.huawei.innovation.rdm.xdm.delegator.ClassificationNodeDelegator;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
-@Api(tags = "Part管理相关接口")
-@RequestMapping
 @RestController
+@RequestMapping("/idme")
+@Api("part相关接口")
 public class PartController {
+
+    @Autowired
+    private ClassificationNodeDelegator classificationNodeDelegator;
+
     @Autowired
     private PartDelegator partDelegator;
 
-
-    public PartViewDTO create(@RequestBody PartDTO partDTO) {
+    @ApiOperation("创建part")
+    @PostMapping("/createPart")
+    public PartViewDTO createPart(){
         PartCreateDTO partCreateDTO = new PartCreateDTO();
-        partCreateDTO.setId(partDTO.getId());
-        partCreateDTO.setModifier(partDTO.getModifier());
-        partCreateDTO.setLastUpdateTime(partDTO.getLast_update_time());
-        partCreateDTO.setCreator(partDTO.getCreator());
-        partCreateDTO.setCreateTime(partDTO.getCreate_time());
-        partCreateDTO.setRdmExtensionType(partDTO.getRdm_ext_type());
-        ObjectReferenceParamDTO tenant = new ObjectReferenceParamDTO(partDTO.getTenant_id(), partDTO.getTenant_clazz());
-        partCreateDTO.setTenant(tenant);
-        partCreateDTO.setName(partDTO.getName());
-        partCreateDTO.setDescription(partDTO.getDescription());
-        partCreateDTO.setSecurityLevel(partDTO.getSecurity_level());
-        partCreateDTO.setKiaguid(partDTO.getKiaguid());
-        partCreateDTO.setWorkingCopy(partDTO.isWorking_copy());
-        partCreateDTO.setCheckOutUserName(partDTO.getCheckout_username());
-        partCreateDTO.setCheckOutTime(partDTO.getCheckout_time());
-        AssemblyMode part_type;
-        String enName = partDTO.getPart_enName();
-        if (enName.equals("Separable")) {
-            part_type = AssemblyMode.Separable;
-        } else if (enName.equals("Inseparable")) {
-            part_type = AssemblyMode.Inseparable;
-        } else {
-            part_type = AssemblyMode.Part;
-        }
-        partCreateDTO.setPartType(part_type);
-        partCreateDTO.setIterationNote(partDTO.getIteration_note());
-        PartSource part_source;
-        String source = partDTO.getSource();
-        if (source.equals("制造") || source.equals("Make")) {
-            part_source = PartSource.Make;
-        } else if (source.equals("购买") || source.equals("Buy")) {
-            part_source = PartSource.Buy;
-        } else {
-            part_source = PartSource.Buy_SingleSource;
-        }
-
-        partCreateDTO.setMaster(getPartMasterCreateDTO(partDTO, tenant));
-        partCreateDTO.setBranch(getPartBranchCreateDTO(partDTO, tenant));
-
+        EXAValueParamDTO exaValueParamDTO = new EXAValueParamDTO();
+      /*  exaValueParamDTO.setName("Classification");
+        exaValueParamDTO.setValue("3_服务器");*/
+        List<EXAValueParamDTO> list = new ArrayList<>();
+        list.add(exaValueParamDTO);
+        partCreateDTO.setExtAttrs(list);
         return partDelegator.create(partCreateDTO);
     }
 
-    private static PartMasterCreateDTO getPartMasterCreateDTO(PartDTO partDTO, ObjectReferenceParamDTO tenant) {
-        PartMasterCreateDTO partMasterCreateDTO = new PartMasterCreateDTO();
-        partMasterCreateDTO.setId(partDTO.getId());
-        partMasterCreateDTO.setModifier(partDTO.getModifier());
-        partMasterCreateDTO.setLastUpdateTime(partDTO.getLast_update_time());
-        partMasterCreateDTO.setCreator(partDTO.getCreator());
-        partMasterCreateDTO.setCreateTime(partDTO.getCreate_time());
-        partMasterCreateDTO.setRdmExtensionType(partDTO.getRdm_ext_type());
-        partMasterCreateDTO.setTenant(tenant);
-        partMasterCreateDTO.setName(partDTO.getName());
-        partMasterCreateDTO.setEndPart(partDTO.isEnd_part());
-        partMasterCreateDTO.setNumber(partDTO.getNumber());
-        partMasterCreateDTO.setPhantomPart(partDTO.isPhantom_part());
-        return partMasterCreateDTO;
+    @PostMapping("/getPart")
+    public PartViewDTO getPart(){
+        PersistObjectIdDecryptDTO persistObjectIdDecryptDTO = new PersistObjectIdDecryptDTO();
+        persistObjectIdDecryptDTO.setId(637655581850611712L);
+
+        PartViewDTO result = partDelegator.get(persistObjectIdDecryptDTO);
+
+        System.out.println(result.toString());
+        return result;
     }
 
-    private static PartBranchCreateDTO getPartBranchCreateDTO(PartDTO partDTO, ObjectReferenceParamDTO tenant) {
-        PartBranchCreateDTO partBranchCreateDTO = new PartBranchCreateDTO();
-        partBranchCreateDTO.setId(partDTO.getId());
-        partBranchCreateDTO.setModifier(partDTO.getModifier());
-        partBranchCreateDTO.setLastUpdateTime(partDTO.getLast_update_time());
-        partBranchCreateDTO.setCreator(partDTO.getCreator());
-        partBranchCreateDTO.setCreateTime(partDTO.getCreate_time());
-        partBranchCreateDTO.setRdmExtensionType(partDTO.getRdm_ext_type());
-        partBranchCreateDTO.setTenant(tenant);
-        return partBranchCreateDTO;
-    }
+
 }
