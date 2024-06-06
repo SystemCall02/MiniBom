ece5149a19524da6bfb6da16fff95ce3d703f024
pan <1131240003@qq.com>
2024-06-06 16:10:11 +0800

    分类树形结构功能实现

---
+++ src/main/java/com/idme/minibom/pojo/VO/ClassificationTreeVO.java
@@ -0,0 +1,16 @@
+package com.idme.minibom.pojo.VO;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ClassificationTreeVO {
+    private Long id;
+    private String name;
+    private String businessCode;
+    private String nameEn;
+    private Boolean enableFlag;
+    private List<ClassificationTreeVO> children;
+}
