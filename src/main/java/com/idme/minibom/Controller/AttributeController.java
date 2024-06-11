6f0a4900364372469ec7ea55d802855b0b406d78
pan <1131240003@qq.com>
2024-06-11 15:09:41 +0800

    属性删除的实现

--- src/main/java/com/idme/minibom/Controller/AttributeController.java
+++ src/main/java/com/idme/minibom/Controller/AttributeController.java
@@ -1,18 +1,23 @@
 package com.idme.minibom.Controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdDecryptDTO;
+import com.huawei.innovation.rdm.coresdk.basic.dto.PersistObjectIdModifierDTO;
 import com.huawei.innovation.rdm.coresdk.basic.enums.ConditionType;
 import com.huawei.innovation.rdm.coresdk.basic.enums.JoinerType;
-import com.huawei.innovation.rdm.coresdk.basic.vo.QueryCondition;
-import com.huawei.innovation.rdm.coresdk.basic.vo.QueryRequestVo;
-import com.huawei.innovation.rdm.coresdk.basic.vo.RDMPageVO;
-import com.huawei.innovation.rdm.xdm.delegator.ClassificationNodeDelegator;
-import com.huawei.innovation.rdm.xdm.delegator.EXADefinitionDelegator;
-import com.huawei.innovation.rdm.xdm.delegator.EXADefinitionLinkDelegator;
+import com.huawei.innovation.rdm.coresdk.basic.vo.*;
+import com.huawei.innovation.rdm.delegate.common.XdmDelegateConsts;
+import com.huawei.innovation.rdm.delegate.service.XdmTokenService;
+import com.huawei.innovation.rdm.xdm.delegator.*;
 import com.huawei.innovation.rdm.xdm.dto.entity.EXADefinitionCreateDTO;
 import com.huawei.innovation.rdm.xdm.dto.entity.EXADefinitionQueryViewDTO;
 import com.huawei.innovation.rdm.xdm.dto.entity.EXADefinitionUpdateDTO;
 import com.huawei.innovation.rdm.xdm.dto.entity.EXADefinitionViewDTO;
+import com.idme.minibom.Config.RestTemplateConfig;
+import com.idme.minibom.Constant.APIConstant;
 import com.idme.minibom.Constant.AttributeConstraintConstant;
+import com.idme.minibom.Constant.ExceptionConstant;
+import com.idme.minibom.Exception.AttributeNotAllowedDeleteException;
 import com.idme.minibom.Result.Result;
 import com.idme.minibom.pojo.DTO.AttrCreateDTO;
 import com.idme.minibom.pojo.DTO.AttrUpdateDTO;
@@ -22,8 +27,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.hibernate.mapping.Join;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -35,7 +45,10 @@ public class AttributeController {
     private EXADefinitionDelegator exaDefinitionDelegator;
 
     @Autowired
-    private ClassificationNodeDelegator classificationNodeDelegator;
+    private XdmTokenService tokenService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
 
     /**
@@ -108,7 +121,7 @@ public class AttributeController {
     }
 
     /**
-     * 更新属性信息，仅和描述
+     * 更新属性信息，仅中英文描述
      * @return
      */
     @PostMapping("/update")
@@ -132,14 +145,39 @@ public class AttributeController {
      * 删除属性，需注意属性不能被任何分类引用
      * @return
      */
-    //TODO 遇上严重问题，无法获取到属性和分类的关联情况，待解决
-/*
-    @DeleteMapping("delete")
+    @DeleteMapping("/delete/{id}")
     @ApiOperation("删除属性")
     public Result deleteAttr(@PathVariable Long id){
-        classificationNodeDelegator.
+        if(!isAttrReferenceNull(id)){
+            throw new AttributeNotAllowedDeleteException(ExceptionConstant.ATTR_DELETE_NOT_ALLOWED);
+        }
+
+        PersistObjectIdModifierDTO persistObjectIdModifierDTO = new PersistObjectIdModifierDTO();
+        persistObjectIdModifierDTO.setId(id);
+        exaDefinitionDelegator.delete(persistObjectIdModifierDTO);
+        return Result.success();
+
     }
-*/
+
+    //@GetMapping("/reference")
+    private boolean isAttrReferenceNull(Long id){
+        String url = APIConstant.ENDPOINT+"//EXADefinition/queryExadefinitionNodeRefered/10/1";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set(XdmDelegateConsts.X_AUTH_TOKEN,tokenService.getToken());
+        //QueryRequestVo queryRequestVo = new QueryRequestVo();
+/*        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id",637593447506649088L);*/
+       // RDMParamVO
+        HashMap<String,Long> params = new HashMap<>();
+        params.put("id",id);
+        HttpEntity<HashMap<String,Long>> request = new HttpEntity<>(params,headers);
+
+        RDMResultVO body = restTemplate.postForEntity(url, request, RDMResultVO.class).getBody();
+
+        return body.getData().isEmpty();
+
+    }
 
 
 }
